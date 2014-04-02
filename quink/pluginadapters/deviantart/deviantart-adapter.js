f18dc83070d40a0d510a78c19458487d949f3450
Phil Westwell <phil.westwell@gmail.com>
2014-04-02 12:07:00 +0100

    Added minimal plugin adapter

---
+++ quink/pluginadapters/deviantart/deviantart-adapter.js
@@ -0,0 +1,73 @@
+/**
+ * Quink, Copyright (c) 2013-2014 IMD - International Institute for Management Development, Switzerland.
+ *
+ * This file is part of Quink.
+ *
+ * Quink is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Quink is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Quink.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*global */
+require([
+    'Underscore',
+    'jquery',
+    'ext/PluginAdapterContext'
+], function (_, $, Context) {
+    'use strict';
+    //the iframe and the imageUploader component
+    var $frameElements,
+        imageUploader,
+        $mask;
+
+    //two finger scroll on trackpad appears as the mousewheel event
+    $mask = $('<div>').addClass('qk_mask')
+        .on('touchstart touchmove touchend click mousewheel', function (event) {
+            event.preventDefault();
+        });
+
+    /**
+     * Plugin API method - see Quink-Plugin-Notes document
+     *
+     */
+    function load() {
+        console.log('[' + new Date().toISOString() + ']' + 'DeviantArtPlugin.load() called');
+    }
+
+    /**
+     * Plugin API method - see Quink-Plugin-Notes document
+     * (i) add the plugin markup to the DOM (re-using any plugin artifacts that have been previously downloaded)
+     * (ii) show the plugin, ready to be used
+     *
+     * @param data - used to supply the open function with any data that is to be used by the plugin.
+     *
+     */
+    function open(data) {
+        console.log('[' + new Date().toISOString() + ']' + 'DeviantArtPlugin.open(data) called');
+    }
+    /**
+     * Plugin API method - see Quink-Plugin-Notes document
+     *
+     * Publish on a saved topic and as part of the publication include the serialised data to be saved
+     */
+    function save() {
+        console.log('[' + new Date().toISOString() + ']' + 'DeviantArtPlugin.save() called');
+    }
+     /**
+     * Plugin API method - see Quink-Plugin-Notes document
+     *
+     * When ready to exit, publish on an exited topic
+     */
+    function exit() {
+        console.log('[' + new Date().toISOString() + ']' + 'DeviantArtPlugin.exit() called');
+    }
+ });
