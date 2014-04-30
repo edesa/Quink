06620c6945b7e5df4ca94e1de162acf6245eba59
Arthur Seaton <arthur@firespritesoftware.com>
2014-04-30 08:43:19 +0100

    Should help to reduce unwanted side effects in the main document when some plugins are active... at least on some platforms.

--- quink/js/main.js
+++ quink/js/main.js
@@ -19,13 +19,13 @@
 
 require.config({
     // Dev only for Mobile Safari to stop it caching the JS.
-    // urlArgs: "bust=" + (new Date()).getTime(),
+    urlArgs: "bust=" + (new Date()).getTime(),
 
     paths: {
         jquery: 'lib/jquery-1.10.2',
         rangy: 'lib/rangy-core',
         textrange: 'lib/rangy-textrange',
-        Underscore: 'lib/underscore'
+        Underscore: 'lib/underscore',
     },
 
     shim: {
