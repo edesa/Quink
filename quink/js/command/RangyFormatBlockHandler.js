69ec34e0988811a5515fb3edced74f0c617f2c99
Arthur Seaton <arthur@firespritesoftware.com>
2014-08-21 11:01:30 +0100

    Makes H<n> work better when applied to a collapsed seection within a list.

--- quink/js/command/RangyFormatBlockHandler.js
+++ quink/js/command/RangyFormatBlockHandler.js
@@ -6,28 +6,38 @@
 
 define([
     'Underscore',
+    'jquery',
     'rangy',
     'cssapplier',
     'util/PubSub'
-], function (_, rangy, cssapplier, PubSub) {
+], function (_, $, rangy, cssapplier, PubSub) {
     'use strict';
 
     var RangyFormatBlockHandler = function () {
         this.createApplierMap();
     };
 
+    RangyFormatBlockHandler.prototype.CSS_CLASSES = [
+        'qk_h1',
+        'qk_h2',
+        'qk_h3',
+        'qk_h4',
+        'qk_h5',
+        'qk_h6'
+    ];
+
     RangyFormatBlockHandler.prototype.createApplierMap = function () {
         this.APPLIER_MAP = {
-            'h1': rangy.createCssClassApplier('qk_h1'),
-            'h2': rangy.createCssClassApplier('qk_h2'),
-            'h3': rangy.createCssClassApplier('qk_h3'),
-            'h4': rangy.createCssClassApplier('qk_h4'),
-            'h5': rangy.createCssClassApplier('qk_h5'),
-            'h6': rangy.createCssClassApplier('qk_h6')
+            'qk_h1': rangy.createCssClassApplier('qk_h1'),
+            'qk_h2': rangy.createCssClassApplier('qk_h2'),
+            'qk_h3': rangy.createCssClassApplier('qk_h3'),
+            'qk_h4': rangy.createCssClassApplier('qk_h4'),
+            'qk_h5': rangy.createCssClassApplier('qk_h5'),
+            'qk_h6': rangy.createCssClassApplier('qk_h6')
         };
     };
 
-    RangyFormatBlockHandler.prototype.handleCollapsedRange = function (range, applier) {
+    RangyFormatBlockHandler.prototype.foo = function (range, applier) {
         var el = document.createElement('span'),
             dummy = document.createTextNode(' '); // webkit hack
         el.appendChild(dummy);
@@ -37,21 +47,42 @@ define([
         rangy.getSelection().setSingleRange(range);
     };
 
+    RangyFormatBlockHandler.prototype.handleCollapsedRange = function (range, applier, cssClass) {
+        var el = range.startContainer;
+        if (el.children && el.children.length === 1) {
+            el = $(el);
+            if (!el.hasClass(cssClass)) {
+                this.removeCssClassesNot(el, cssClass);
+                el.addClass(cssClass);
+            }
+        } else {
+            this.foo(range, applier);
+        }
+    };
+
+    RangyFormatBlockHandler.prototype.removeCssClassesNot = function (el, cssClass) {
+        var others = _.without(this.CSS_CLASSES, cssClass);
+        others.forEach(function (cls) {
+            el.removeClass(cls);
+        });
+    };
+
     /**
      * Make sure that all the other possible css classes aren't applied to the selection before applying
      * the new one. This prevents one element having a number of css classes applied to it at the same time.
      */
-    RangyFormatBlockHandler.prototype.applyCssClass = function(args) {
-        var applier = this.APPLIER_MAP[args],
+    RangyFormatBlockHandler.prototype.applyCssClass = function (header) {
+        var cssClass = 'qk_' + header,
+            applier = this.APPLIER_MAP[cssClass],
             range, others;
         if (applier) {
             range = rangy.getSelection().getRangeAt(0);
             if (range.collapsed) {
-                this.handleCollapsedRange(range, applier);
+                this.handleCollapsedRange(range, applier, cssClass);
             } else {
                 others = _.values(this.APPLIER_MAP);
                 others.splice(others.indexOf(applier), 1);
-                others.forEach(function(applr) {
+                others.forEach(function (applr) {
                     applr.undoToSelection();
                 });
                 applier.toggleSelection();
