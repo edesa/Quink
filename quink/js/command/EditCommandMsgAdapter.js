a28a544971ea9c065244d9b792a258d8108d1716
Arthur Seaton <arthur@firespritesoftware.com>
2014-08-21 15:17:30 +0100

    Revert the H<n> commands to use the browser's execCommand in all cases. Remove the quink format block handler but leave the rangy one in to be used as the basis for setting css classes onto selections.

--- quink/js/command/EditCommandMsgAdapter.js
+++ quink/js/command/EditCommandMsgAdapter.js
@@ -5,76 +5,33 @@
  */
 
 define([
-    'command/EditCommandHandler',
-    'command/QuinkFormatBlockHandler',
-    'command/RangyFormatBlockHandler',
-    'util/Env'
-], function (EditCommandHandler, QuinkFormatBlockHandler, RangyFormatBlockHandler, Env) {
+    'command/EditCommandHandler'
+], function (EditCommandHandler) {
     'use strict';
 
     var EditCommandMsgAdapter = function () {
         this.handler = new EditCommandHandler();
-        this.formatBlockHandler = this.getFormatBlockHandler();
     };
 
-    EditCommandMsgAdapter.prototype.EDIT_HANDLER_IDS = [
+    EditCommandMsgAdapter.prototype.ACCEPTED_IDS = [
         'edit',
         'font',
-        'para'
+        'para',
+        'style'
     ];
-
-    EditCommandMsgAdapter.prototype.getFormatBlockHandler = function () {
-        var handler = this.handler,
-            name = Env.getParam('formatblock', 'browser').toLowerCase();
-        if (name === 'quink') {
-            console.log('QuinkFormatBlockHandler');
-            handler = new QuinkFormatBlockHandler();
-        } else if (name === 'rangy') {
-            handler = new RangyFormatBlockHandler();
-            console.log('RangyFormatBlockHandler');
-        } else {
-            handler = this.handler;
-        }
-        return handler;
-    };
-
-    EditCommandMsgAdapter.prototype.getHandler = function (ar) {
-        var id = ar[0].toLowerCase(),
-            cmd, handler;
-        if (this.EDIT_HANDLER_IDS.indexOf(id) >= 0) {
-            handler = this.handler;
-        } else if (id === 'style') {
-            cmd = ar[1].toLowerCase();
-            if (cmd === 'formatblock' && /^h[1-6]$/.test(ar[2])) {
-                handler = this.formatBlockHandler;
-            } else {
-                handler = this.handler;
-            }
-        }
-        return handler;
-    };
 
     EditCommandMsgAdapter.prototype.handle = function (msg) {
         var ar = msg.split('.'),
-            handler = this.getHandler(ar);
-        if (handler) {
-            handler.execCmd(ar[1], ar[2] || null);
+            id = ar[0],
+            cmd = ar[1],
+            args = ar[2] || null,
+            handled;
+        if (this.ACCEPTED_IDS.indexOf(id.toLowerCase()) >= 0) {
+            this.handler.execCmd(cmd, args);
+            handled = true;
         }
-        return !!handler;
+        return handled;
     };
-
-    // EditCommandMsgAdapter.prototype.handle = function (msg) {
-    //     var ar = msg.split('.'),
-    //         id = ar[0],
-    //         cmd = ar[1],
-    //         args = ar[2] || null,
-    //         handled;
-    //     if (this.ACCEPTED_IDS.indexOf(id.toLowerCase()) >= 0) {
-    //         this.handler.execCmd(cmd, args);
-    //         handled = true;
-    //     }
-    //     return handled;
-    // };
 
     var instance;
 
