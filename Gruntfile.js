1f6c637de8c04cd3171724782af64212c4fbca4e
Arthur Seaton <arthur@firespritesoftware.com>
2014-10-29 09:06:09 +0000

    Changes to the build process. The final built app (main.js) no longer includes the third party libs. Libs use the minified versions for the build and full versions for dev. The build copies over the minified libs and renames them to match the require.js paths. Don't include the text plugin in the final built app as it's not needed. The require.js optimiser inlines resources that the text plugin handles. Provide a more suitable banner for built app that includes the app name, IMD attribution and a link to the license. This is better than the previously used src banners. Use the same banner for both the bootstrap and the app. Include the html examples in the built directory. This makes it easier to test the built app in isolation to make sure it's all working. It also easy to leave the examples behind when using the built app in another context so there is no real down side.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,24 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        banner: '/**\n' +
+            ' * <%= pkg.name %>\n' +
+            ' *\n' +
+            ' * Copyright (c), 2013-2014 IMD - International Institute for Management Development, Switzerland.\n' +
+            ' * <%= pkg.licenses[0].url %>\n' +
+            ' */\n',
 
+        /**
+         * The text plugin has to be in place for the require.js optimiser to work but isn't needed
+         * at runtime because the resources it handles are inlined by the optimiser.
+         */
         clean: {
-            production: ['prebuilt']
+            all: ['prebuilt'],
+            text: ['prebuilt/quink/js/lib/text.js']
         },
 
         copy: {
-            production: {
+            all: {
                 files: [{
                     expand: true,
                     src: [
@@ -17,59 +28,101 @@ module.exports = function (grunt) {
                         'readonly.html',
                         'index.html',
                         'quink.html',
-                        'quink/js/lib/require.js',
                         'quink/plugins/**',
                         'quink/resources/**',
                         '!quink/resources/css/**',
-                        'quink/pluginadapters/**'
+                        'quink/pluginadapters/**',
+                        'examples/html/**'
                     ],
                     dest: 'prebuilt'
+                }]
+            },
+            /**
+             * The expanded version of the libs are used in dev. For a build the minified versions are copied
+             * across and renamed to match the dev versions so that the require.js paths work.
+             * Minified libs must have names that follow a <foo>[.-]min.js pattern.
+             */
+            libs: {
+                files: [{
+                    expand: true,
+                    cwd: 'quink/js/lib',
+                    src: [
+                        '*[\\.-]min.js'
+                    ],
+                    dest: 'prebuilt/quink/js/lib/',
+                    rename: function (dest, src) {
+                        return dest + src.replace(/[\.\-]min/, '');
+                    }
                 }]
             }
         },
 
+        /**
+         * Don't include the libs in the built app. Libs are handled separately. The text plugin has to be
+         * present for the build to work which is why it's handled differently.
+         * In order to make the quink.js bootstrap file have exactly the same banner as the built app don't
+         * use the src banners. The banner will be appended in a separate step after the app is built.
+         */
         requirejs: {
-            production: {
+            all: {
                 options: {
                     baseUrl: 'quink/js',
-                    mainConfigFile: 'quink/js/main.js',
                     name: 'main',
                     include: 'ext/PluginAdapterContext',
-                    out: 'prebuilt/quink/js/main.js'
-                }
-            }
-        },
-
-        mkdir: {
-            production: {
-                options: {
-                    create: ['prebuilt/quink/js']
+                    out: 'prebuilt/quink/js/main.js',
+                    paths: {
+                        jquery: 'empty:',
+                        rangy: 'empty:',
+                        textrange: 'empty:',
+                        cssapplier: 'empty:',
+                        Underscore: 'empty:',
+                        text: 'lib/text',
+                        resources: '../resources'
+                    },
+                    stubModules: ['text'],
+                    preserveLicenseComments: false
                 }
             }
         },
 
         cssmin: {
-            production: {
+            all: {
                 src: 'quink/resources/css/quink.css',
                 dest: 'prebuilt/quink/resources/css/quink.css'
             }
         },
 
         uglify: {
-            production: {
+            all: {
+                options: {
+                    banner: '<%= banner %>'
+                },
                 files: {
                     'prebuilt/quink.js': ['quink.js']
                 }
             }
+        },
+
+        /**
+         * Ensures that the built app has the same banner as the bootstrap.
+         */
+        concat: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            all: {
+                src: ['prebuilt/quink/js/main.js'],
+                dest: 'prebuilt/quink/js/main.js'
+            }
         }
     });
 
-    grunt.loadNpmTasks('grunt-mkdir');
     grunt.loadNpmTasks('grunt-css');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
-    grunt.registerTask('default', ['clean', 'copy', 'mkdir', 'requirejs', 'cssmin', 'uglify']);
+    grunt.registerTask('default', ['clean:all', 'copy:all', 'copy:libs', 'requirejs', 'concat', 'cssmin', 'uglify', 'clean:text']);
 };
