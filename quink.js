98f51bd7c240761ee004354c15ba4af7cd1ac4bb
Arthur Seaton <arthur@firespritesoftware.com>
2015-02-25 12:00:20 +0000

    Changes to the bootstrap so that it can handle the various use cases. These are the previous scenarios where quick is used by a non AMD app and the new scenario where quick is loaded as an AMD module by an AMD app. In addition both scenarios need to work in both dev and production without source code changes. I doubt this is the best or even final solution but it seems to work for now.

--- quink.js
+++ quink.js
@@ -7,7 +7,7 @@
 /**
  * To use Quink as a component:
  *      - include the prebuilt/quink directory
- *      - load this script
+ *      - load this script which must be in the same directory as quink
  */
 (function (global) {
     'use strict';
@@ -17,21 +17,40 @@
         JSLIB_DIR = JS_DIR + '/lib',
         RES_DIR = QUINK_ROOT + '/resources',
         CSS_DIR = RES_DIR + '/css',
+        isAmdLoad = false,
         root;
+
+    /**
+     * Very simple check to see if there's a url query parameter with the given name. Value, including
+     * the '=' is optional.
+     */
+    function hasQueryParam(name) {
+        return new RegExp('[?&]' + name + '=?').test(location.search);
+    }
 
     function getDir(dir) {
         var r = root || './';
         return r + dir;
     }
 
+    /**
+     * Loads main.js directly. This assumes main.js is the result of a build that will include an AMD loader.
+     */
     function loadScripts() {
+        var script = document.createElement('script'),
+            head = document.getElementsByTagName('head')[0];
+        script.setAttribute('src', getDir(JS_DIR) + '/main.js');
+        head.appendChild(script);
+    }
+
+    /**
+     * Loads main.js using require.js.
+     */
+    function loadRequireScripts() {
         var script = document.createElement('script'),
             head = document.getElementsByTagName('head')[0];
         script.setAttribute('data-main', getDir(JS_DIR) + '/main.js');
         script.setAttribute('src', getDir(JSLIB_DIR) + '/require.js');
-        script.onload = function () {
-            console.log('Loaded require.js');
-        };
         head.appendChild(script);
     }
 
@@ -40,23 +59,26 @@
             link = document.createElement('link');
         link.setAttribute('rel', 'stylesheet');
         link.setAttribute('href', getDir(CSS_DIR) + '/quink.css');
-        link.onload = function () {
-            console.log('Loaded css');
-        };
         head.appendChild(link);
     }
 
+    /**
+     * Works out the root directory for the quink distribution. Additionally decides whether to load scripts
+     * or to leave that to require.js.
+     */
     function calcRoot() {
         var scripts = document.querySelectorAll('script'),
             len = scripts.length,
-            i, src, qi;
+            i, src, qi, script;
         for (i = 0; i < len; i++) {
-            src = scripts[i].getAttribute('src');
+            script = scripts[i];
+            src = script.getAttribute('src');
             if (src) {
                 qi = src.indexOf('quink.js');
                 if (qi >= 0) {
                     root = src.substr(0, qi);
                     global.QUINK.root = root;
+                    isAmdLoad = !!script.getAttribute('data-amdload');
                     break;
                 }
             }
@@ -66,6 +88,12 @@
     global.QUINK = global.QUINK || {};
     calcRoot();
     loadStyleSheet();
-    loadScripts();
+    if (!isAmdLoad) {
+        if (hasQueryParam('userequire')) {
+            loadRequireScripts();
+        } else {
+            loadScripts();
+        }
+    }
 
 }(window));
